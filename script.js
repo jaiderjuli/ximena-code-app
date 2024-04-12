daffd9626fce1443c0cb04f5b6d3c6876a3ae1fe
julian rodriguez <jaiderjulianr@gmail.com>
2024-04-12 10:44:00 -0500

    masterp

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const buttonSi = document.getElementById('yes');
       const nombre = prompt("Por favor, ingresa tu nombre:");
       if (nombre) {
         alert(`Hola, ${nombre}! NOS ALEGRA QUE NOS PUEDAS ACOMPAÑAR😊 (ya sabe que está comprometido con Dios🫵)`);
-        alert('🤩a las 6:00pm te esperamos😎');
+        alert('🤩a las 6:00pm te esperamos😎 App Ximena');
         location.href = 'https://www.youtube.com/watch?v=QKD2Bb17ONA=1:54s';
       } else {
         alert("Por favor, ingresa un nombre válido.");
